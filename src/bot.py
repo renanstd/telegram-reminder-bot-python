3446357fcc4d0e1dfa52b71ab4a9e79ebe536e01
Renan Santana <renan_ota@hotmail.com>
2021-03-22 22:21:51 -0300

    Me ajuda socorro

--- src/bot.py
+++ src/bot.py
@@ -27,8 +27,6 @@ if ENVIRONMENT == 'dev':
     updater.start_polling()
     updater.idle()
 else:
-    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-    print(PORT)
     updater.start_webhook(
         listen='0.0.0.0',
         port=PORT,
