bc7888024f5c2c39fcd25f02d12a30c5a8890b15
Renan Santana <renan_ota@hotmail.com>
2021-03-25 23:49:40 -0300

    Implementa verificador de mensagens

---
+++ src/checker.py
@@ -0,0 +1,22 @@
+import datetime
+from database import init_database
+from models import Reminder
+from telegram import Bot
+from settings import BOT_TOKEN
+
+
+# Inicializa db e bot
+init_database()
+bot = Bot(token=BOT_TOKEN)
+
+# Busca lembretes que deverão ser enviados
+reminders = Reminder.select().where(
+    Reminder.datetime <= datetime.datetime.now(),
+    Reminder.done == False
+)
+
+# Envia as mensagens via telegram
+for reminder in reminders:
+    bot.send_message(reminder.chat_id, reminder.reminder)
+    reminder.done = True
+    reminder.save()
