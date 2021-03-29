5b073f5187aedf49d0b9369b65f387640944c484
Renan Santana <renan_ota@hotmail.com>
2021-03-29 01:16:14 -0300

    Prepara regex das mensagens

--- src/bot_handlers/messages.py
+++ src/bot_handlers/messages.py
@@ -1,3 +1,4 @@
+import re
 from telegram import Update
 from telegram.ext import CallbackContext
 from models import Reminder
@@ -6,4 +7,14 @@ from models import Reminder
 def handle_message(update: Update, _: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     message = update.message.text
-    update.message.reply_text(message)
+
+    regex_date = re.compile('\d{2}\/\d{2}\/\d{4}')
+    regex_tomorrow = re.compile('amanhã|amanha')
+    regex_today = re.compile('hoje')    
+    
+    date = regex_date.findall(message)
+    tomorrow = regex_tomorrow.findall(message)
+    today = regex_today.findall(message)
+
+    # message = "Ok, te lembrarei de {}, no dia {} as {}.".format()
+    update.message.reply_text(date + tomorrow + today)
